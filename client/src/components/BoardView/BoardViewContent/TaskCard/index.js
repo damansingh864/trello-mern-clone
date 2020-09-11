81077497b7025189e21cacb178177033675692bf
Damanvir Singh <damansingh864@gmail.com>
2020-09-12 00:18:31 +0530

    Modify Card Display

--- client/src/components/BoardView/BoardViewContent/TaskCard/index.js
+++ client/src/components/BoardView/BoardViewContent/TaskCard/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
-import { Wrapper } from './styles'
+import { Wrapper, Title, Description, CardBottom, TeamList, CardActivity, IconWrap, ImageWrap } from './styles'
+
+const data = [
+  {
+    name: 'Old fashioned recipe for preventing allergies and chemicals',
+    image: 'ad',
+    description: "This is our Description",    
+    activity: {
+      comments: 42,
+      favorites: 20,
+      attachments: 10
+    }
+  }
+]
 
 const TaskCard = () => {
   return (
-    <Wrapper>
-      asdasdasd
+    <Wrapper draggable>
+      {data[0].image && <ImageWrap></ImageWrap>}
+      <Title>{data[0].name}</Title>
+      {data[0].description && <Description>{data[0].description}</Description>}
+      <CardBottom>
+        <TeamList>
+        </TeamList>
+        <CardActivity>
+          {data[0].activity.comments && <IconWrap>{data[0].activity.comments} <i class="far fa-heart" /></IconWrap>}
+          {data[0].activity.favorites && <IconWrap>{data[0].activity.favorites} <i class="far fa-comment-dots" /></IconWrap>}
+          {data[0].activity.attachments && <IconWrap>{data[0].activity.attachments} <i class="fas fa-paperclip" /></IconWrap>}
+        </CardActivity>
+      </CardBottom>
     </Wrapper>
   )
 }
--- client/src/components/BoardView/BoardViewContent/TaskCard/styles.js
+++ client/src/components/BoardView/BoardViewContent/TaskCard/styles.js
@@ -2,11 +2,60 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  /* justify-content: space-between; */
   width: 100%;
   background: #eee;
-  align-items: center;
+  /* align-items: center; */
   border-radius: 8px;
-  padding: 6px 15px;  
-  margin-top: 10px
+  padding: 15px 15px;  
+  margin-top: 10px;
+
+  &:hover {
+    box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+`
+
+export const ImageWrap = styled.div`
+  border: 1px solid #555;
+  height: 110px;
+  width: 100%;
+  border-radius: 8px;
+  margin-bottom: 15px;
+  background: #555;
+`
+
+export const Title = styled.div`
+  font-size: 14px;
+  color: #444;
+  font-weight: 600;
+  margin-bottom: 10px;
+`
+
+export const Description = styled.div`
+  font-size: 12px;
+  color: #888;
+  margin-bottom: 18px;
+`
+
+export const CardBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const TeamList = styled.div`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #444;
+`
+
+export const CardActivity = styled.div`
+`
+
+export const IconWrap = styled.span`
+  font-size: 12px;
+  color: #aaa;
+  margin-left: 10px;
 `
